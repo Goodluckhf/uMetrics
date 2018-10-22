90a44ac3ede63614f923e698ffaa5ddee0db1c7b
Konstantin <goodluckhf@yandex.ru>
2018-10-22 19:44:50 +0300

    feat(UMetrics.js): Add Facade for control metrics

---
+++ index.js
@@ -0,0 +1,11 @@
+import UMetrics      from './UMetrics';
+import PullTransport from './transport/PullTransport';
+import PushTransport from './transport/PushTransport';
+import GaugeMetric   from './metric/GaugeMetric';
+
+export {
+	UMetrics,
+	PushTransport,
+	PullTransport,
+	GaugeMetric,
+};
